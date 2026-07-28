0fe266c970514cd71b045eb8c86c219d7f90fd0f
Umang Mathur <mathurumang845@gmail.com>
2026-07-28 13:14:48 +0530

    Time: 185 ms (68.20%) | Memory: 21 MB (51.15%) - LeetSync

--- 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.py
+++ 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.py
@@ -2,15 +2,12 @@ from collections import Counter
 class Solution:
     def smallestPalindrome(self, s: str) -> str:
         count = Counter(s)
-
         left = ""
-        middle = ""
-
+        middle=""
         for ch in sorted(count):
-            left += ch *(count[ch]//2)
+            left += ch * (count[ch]//2)
 
-            if count[ch]%2==1:
-                middle += ch
+            if count[ch] % 2 ==1:
+                middle = ch
         return left+middle+left[::-1]
-
         
