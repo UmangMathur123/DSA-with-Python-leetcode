a27b0450b5dae714ed033cdc0ec324bf2dac4410
Umang Mathur <mathurumang845@gmail.com>
2026-08-05 23:10:34 +0530

    Time: 288 ms (69.70%) | Memory: 145.8 MB (30.30%) - LeetSync

---
+++ 3561-remove-methods-from-project/remove-methods-from-project.py
@@ -0,0 +1,31 @@
+from typing import List
+from collections import defaultdict
+
+class Solution:
+    def remainingMethods(self, n: int, k: int, invocations: List[List[int]]) -> List[int]:
+
+        graph = defaultdict(list)
+
+        for u, v in invocations:
+            graph[u].append(v)
+
+        suspicious = [False] * n
+
+        def dfs(u):
+            suspicious[u] = True
+            for v in graph[u]:
+                if not suspicious[v]:
+                    dfs(v)
+
+        dfs(k)
+
+        for u, v in invocations:
+            if not suspicious[u] and suspicious[v]:
+                return list(range(n))
+
+        ans = []
+        for i in range(n):
+            if not suspicious[i]:
+                ans.append(i)
+
+        return ans
